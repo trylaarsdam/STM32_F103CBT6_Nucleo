0fbea54867b6fc7fb14896cc1d99c7d3bf198b80
Todd Rylaarsdam <todd@toddr.org>
2021-08-04 15:50:08 -0500

    updates

--- STM32mainController/src/main.cpp
+++ STM32mainController/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include "STM32LowPower.h"
+
 #define HW_UART_TX D1
 #define HW_UART_RX D0
 
@@ -7,12 +9,18 @@ HardwareSerial SerialAT(HW_UART_RX, HW_UART_TX);
 void setup() {
   // put your setup code here, to run once
   SerialAT.begin(9600);
+  LowPower.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  SerialAT.println("AT+JOIN=0");
-  delay(200);
+  SerialAT.println("ATZ");
+  delay(1000);
+  SerialAT.println("AT+BAND=8");
+  delay(1000);
+  SerialAT.println("AT+JOIN=0"); 
+  delay(3000);
   SerialAT.println("AT+SEND=1:0:ABCDEF");
-  delay(200);
+  delay(500);
+  LowPower.shutdown(20000);
 }
